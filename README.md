880552a168c08af334b5e5d8868bfbe5ea881445
Costa Huang <costa.huang@outlook.com>
2022-07-15 11:03:53 -0400

    update instructions

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# envpool-cleanrl
+# EnvPool + CleanRL
+
+This repo contains the source code of the CleanRL experiments presented in [**EnvPool: A Highly Parallel Reinforcement Learning Environment Execution Engine**](https://arxiv.org/abs/2206.10558)
+
+* Paper url: https://arxiv.org/abs/2206.10558
+* Tracked Weights and Biases experiments: https://wandb.ai/openrlbenchmark/envpool-cleanrl
+
+If you like this repo, consider checking out CleanRL (https://github.com/vwxyzjn/cleanrl), the RL library that we used to build this repo.
+
+
+## Get started
+
+Prerequisites:
+* Python 3.9+
+* [Poetry](https://python-poetry.org)
+
+Install dependencies:
+```
+poetry install
+```
+
+
+### Atari
+Train agents:
+```
+poetry run python ppo_atari.py
+```
+Train agents with experiment tracking:
+```
+poetry run python ppo_atari.py --track
+```
+
+
+### MuJoCo
+Train agents:
+```
+poetry run python ppo_continuous_action.py
+```
+Train agents with experiment tracking:
+```
+poetry run python ppo_continuous_action.py --track
+```
+
+## Replicating results from the paper
+
+See `benchmark.sh`
+
+## Citation
+
+```bibtex
+@article{envpool,
+  title={EnvPool: A Highly Parallel Reinforcement Learning Environment Execution Engine},
+  author={Weng, Jiayi and Lin, Min and Huang, Shengyi and Liu, Bo and Makoviichuk, Denys and Makoviychuk, Viktor and Liu, Zichen and Song, Yufan and Luo, Ting and Jiang, Yukun and Xu, Zhongwen and Yan, Shuicheng},
+  journal={arXiv preprint arXiv:2206.10558},
+  year={2022}
+}
+```
--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,23 @@
-xvfb-run -a python -m cleanrl_utils.benchmark \
-    --env-ids pong_v3 surround_v2 tennis_v3  \
-    --command "poetry run python cleanrl/ppo_pettingzoo_ma_atari.py --track --capture-video" \
-    --num-seeds 3 \
-    --workers 3
+xvfb-run -a python benchmark.py \
+    --env-ids Breakout-v5 Pong-v5 \
+    --command "poetry run python ppo_atari_envpool.py --track --wandb-project-name envpool-cleanrl" \
+    --num-seeds 5 \
+    --workers 1
+
+xvfb-run -a python benchmark.py \
+    --env-ids BreakoutNoFrameskip-v4 PongNoFrameskip-v4 \
+    --command "poetry run python ppo_atari.py --track --wandb-project-name envpool-cleanrl" \
+    --num-seeds 5 \
+    --workers 1
+
+xvfb-run -a python benchmark.py \
+    --env-ids Ant-v4 HalfCheetah-v4 Walker2d-v4 Humanoid-v4 \
+    --command "poetry run python ppo_continuous_action_envpool.py --total-timesteps 10000000 --num-envs 64 --anneal-lr=True --clip-vloss=False --ent-coef=0 --learning-rate=0.00295 --max-grad-norm=3.5 --num-minibatches=4 --num-steps=64 --update-epochs=2 --vf-coef=1.3 --track --wandb-project-name envpool-cleanrl" \
+    --num-seeds 5 \
+    --workers 1
+
+OMP_NUM_THREADS=2 python benchmark.py \
+    --env-ids Ant-v2 HalfCheetah-v2 Walker2d-v2 Humanoid-v2 \
+    --command "poetry run python ppo_continuous_action.py --total-timesteps 10000000 --num-envs 64 --anneal-lr=True --clip-vloss=False --ent-coef=0 --learning-rate=0.00295 --max-grad-norm=3.5 --num-minibatches=4 --num-steps=64 --update-epochs=2 --vf-coef=1.3 --track --wandb-project-name envpool-cleanrl" \
+    --num-seeds 5 \
+    --workers 1
